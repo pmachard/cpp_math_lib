a701f00715a872de94b8df17737b0f546419f502
machard <pascal.machard@gmail.com>
2015-03-20 17:13:15 +0100

    Update SimpleVector2D.h
    Add inline

--- SimpleVector2D.h
+++ SimpleVector2D.h
@@ -5,7 +5,7 @@ class SimpleVector2D
     double m_y;
     
   public:
-    SimpleVector2D(void) : m_x(0.0),m_y(0.0)
-    {
-    }
+    inline SimpleVector2D(void) : m_x(0.0),m_y(0.0) {}
+    
+    inline SimpleVector2D(double p_x, double p_y) : m_x(p_x),m_y(p_y) {}
 };
