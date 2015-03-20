91efa1ab9e0cc85db9a39c8ab8c72ec1c82371f5
machard <pascal.machard@gmail.com>
2015-03-20 17:15:37 +0100

    Update SimpleVector2D.h
    Add Get and Set

--- SimpleVector2D.h
+++ SimpleVector2D.h
@@ -6,6 +6,11 @@ class SimpleVector2D
     
   public:
     inline SimpleVector2D(void) : m_x(0.0),m_y(0.0) {}
-    
     inline SimpleVector2D(double p_x, double p_y) : m_x(p_x),m_y(p_y) {}
+    
+    inline double GetX(void) { return m_x; }
+    inline double GetY(void) { return m_y; }
+    inline void SetX(double p_value) { m_x = p_value; }
+    inline void SetY(double p_value) { m_y = p_value; }
+    
 };
