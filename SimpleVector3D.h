fa8c254be48a487beb1aa40b2b374cbb99ba2954
machard <pascal.machard@gmail.com>
2015-04-10 17:25:41 +0200

    Update SimpleVector3D.h

--- SimpleVector3D.h
+++ SimpleVector3D.h
@@ -1,42 +1,81 @@
 #pragma once
+
 namespace prj {
 namespace math {
+	
+/// @class mathematical vector with double (x,y,z).
 class SimpleVector3D
 {
 private:
+	/// The x component.
 	double m_x;
+	/// The y component.
 	double m_y;
+	/// The z component.
 	double m_z;
 public:
+	/// A default constructor.
+	/// @brief x and y and z are set with 0.0
 	inline SimpleVector3D(void) : m_x(0.0), m_y(0.0), m_z(0.0) {}
-	inline SimpleVector3D(const double & p_x, const double & p_y, const double & p_z) : m_x(p_x), m_y(p_y), m_z(p_z) {}
-	inline SimpleVector3D(const SimpleVector2D & p_source)
+
+	/// A constructor with parameter.
+	/// @param x value
+	/// @param y value	
+	inline SimpleVector3D(const double & x, const double & y, const double & z) : m_x(x), m_y(y), m_z(z) 
+	{}
+	
+	/// A copy constructor.
+	/// @param source the copy source
+	inline SimpleVector3D(const SimpleVector3D & source)
 	{
-		m_x = p_source.m_x;
-		m_y = p_source.m_y;
-		m_z = p_source.m_z;
+		m_x = source.m_x;
+		m_y = source.m_y;
+		m_z = source.m_z;
 	}
 	
-	inline SimpleVector3D & operator = (const SimpleVector3D & p_source)
+	/// A affection operator.
+	/// @param p_source the affectation source
+	/// @return the object with new value
+	inline SimpleVector3D & operator = (const SimpleVector3D & source)
 	{
-		m_x = p_source.m_x;
-		m_y = p_source.m_y;
-		m_z = p_source.m_z;
+		m_x = source.m_x;
+		m_y = source.m_y;
+		m_z = source.m_z;
 		return (*this);
 	}
 	
+	/// A field x accessor.
+	/// @return x value
 	inline const double & GetX(void) const { return m_x; }
-	inline const double & GetY(void)const { return m_y; }
-	inline const double & GetZ(void)const { return m_z; }
+	/// Set the field x.
+	/// @param value the value set to x
 	inline void SetX(const double & p_value) { m_x = p_value; }
+	
+	/// A field y accessor.
+	/// @return y value
+	inline const double & GetY(void)const { return m_y; }
+	/// Set the field y.
+	/// @param value the value set to y
 	inline void SetY(const double & p_value) { m_y = p_value; }
+	
+	/// A field z accessor.
+	/// @return z value
+	inline const double & GetZ(void)const { return m_z; }
+	/// Set the field z.
+	/// @param value the value set to z
 	inline void SetZ(const double & p_value) { m_z = p_value; }
+	
 
+	/// Compute the abslute vector 3d 
+	/// @result the result vector abs(x,y,z) = (abs(x),abs(y),abs(z))
 	inline SimpleVector3D abs (void) const
 	{
 		return SimpleVector3D(fabs(m_x),fabs(m_y),fabs(m_z));
 	}
 	
+	/// Operator += increment vector with another vector
+	/// @param vector to use for additional operation
+	/// @result (xa,ya,za)+=(xb,yb,zb) => (xa+=xb,ya+=yb,za+=zb)
 	inline SimpleVector3D & operator += (const SimpleVector3D & p_source)
 	{
 		m_x += p_source.m_x;
@@ -45,11 +84,17 @@ public:
 		return (*this);
 	}
 	
+	/// Operator + add vector with another vector
+	/// @param vector to use for additional operation
+	/// @result (xa,ya,za)+(xb,yb,zb) => (xa+xb,ya+yb,za+zb)
 	inline SimpleVector3D operator + (const SimpleVector3D & p_source) const
 	{
 		return p_source + (*this);
 	}
 	
+	/// Operator += decrement vector with another vector
+	/// @param vector to use for sustration operation
+	/// @result (xa,ya,za)-=(xb,yb,zb) => (xa-=xb,ya-=yb,za-=zb)
 	inline SimpleVector3D & operator -= (const SimpleVector3D & p_source)
 	{
 		m_x -= p_source.m_x;
@@ -58,14 +103,12 @@ public:
 		return (*this);
 	}
 	
+	/// Operator - sustraction vector with another vector
+	/// @param vector to use for sustraction operation
+	/// @result (xa,ya,za)-(xb,yb,zb) => (xa-xb,ya-yb,za-zb)
 	inline SimpleVector3D operator - (const SimpleVector3D & p_source) const
 	{
 		return p_source - (*this);
-	}
-	
-	inline SimpleVector3D operator * (const double & p_value) const
-	{
-		return SimpleVector3D(m_x*p_value, m_y*p_value, m_z*p_value);
 	}
 	
 	inline SimpleVector2D & operator *= (const double & p_value)
@@ -76,11 +119,9 @@ public:
 		return (*this);
 	}
 	
-	inline SimpleVector3D operator / (const double & p_value) const
+	inline SimpleVector3D operator * (const double & p_value) const
 	{
-		if (p_value == 0.0)
-			throw;
-		return SimpleVector3D(m_x / p_value, m_y / p_value, m_z / p_value);
+		return SimpleVector3D(m_x*p_value, m_y*p_value, m_z*p_value);
 	}
 	
 	inline SimpleVector3D & operator /= (const double & p_value)
@@ -91,6 +132,13 @@ public:
 		m_y /= p_value;
 		m_z /= p_value;
 		return (*this);
+	}
+	
+	inline SimpleVector3D operator / (const double & p_value) const
+	{
+		if (p_value == 0.0)
+			throw;
+		return SimpleVector3D(m_x / p_value, m_y / p_value, m_z / p_value);
 	}
 	
 	inline SimpleVector3D operator * (const SimpleVector3D & p_source) const
