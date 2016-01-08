6f12f93461081224fa7bc6421fd5ef2fa5a27b76
machard <pascal.machard@gmail.com>
2016-01-08 17:01:51 +0100

    Update Vector3D.h

--- Vector3D.h
+++ Vector3D.h
@@ -1,8 +1,12 @@
 #pragma once
 
-namespace prj {
-	namespace math {
+#include "stdafx.h"
+#include <ostream>
 
+namespace pma
+{
+	namespace math
+	{
 		/// @class mathematical vector with T (x,y,z).
 		template <typename T> class Vector3D
 		{
@@ -57,20 +61,20 @@ namespace prj {
 					return GetY();
 				return GetZ();
 			};
-			
-			inline const T& operator[] (unsigned int paramIndex) const 
+
+			inline const T& operator[] (unsigned int paramIndex) const
 			{
 				if (paramIndex == 0)
 					return GetX();
 				if (paramIndex == 1)
 					return GetY();
 				return GetZ();
-			};		
+			};
 			/// Compute the abslute vector 3d 
 			/// @result the result vector abs(x,y,z) = (abs(x),abs(y),abs(z))
-			inline Vector3D<T> abs(void) const 
-			{ 
-				return Vector3D<T>(abs(m_x), abs(m_y), abs(m_z)); 
+			inline Vector3D<T> abs(void) const
+			{
+				return Vector3D<T>(abs(m_x), abs(m_y), abs(m_z));
 			}
 
 			/// Operator += increment vector with another vector
@@ -87,9 +91,9 @@ namespace prj {
 			/// Operator + add vector with another vector
 			/// @param vector to use for additional operation
 			/// @result (xa,ya,za)+(xb,yb,zb) => (xa+xb,ya+yb,za+zb)
-			inline Vector3D<T> operator + (const Vector3D<T> & paramVector) const 
-			{ 
-				return Vector3D<T>(m_x + paramVector.m_x, m_y + paramVector.m_y, m_z + paramVector.m_z); 
+			inline Vector3D<T> operator + (const Vector3D<T> & paramVector) const
+			{
+				return Vector3D<T>(m_x + paramVector.m_x, m_y + paramVector.m_y, m_z + paramVector.m_z);
 			}
 
 			/// Operator += decrement vector with another vector
@@ -106,14 +110,14 @@ namespace prj {
 			/// Operator - sustraction vector with another vector
 			/// @param vector to use to sustraction operation
 			/// @result (xa,ya,za)-(xb,yb,zb) => (xa-xb,ya-yb,za-zb)
-			inline Vector3D<T> operator - (const Vector3D<T> & paramVector) const 
-			{ 
-				return Vector3D<T>(m_x - paramVector.m_x, m_y - paramVector.m_y, m_z - paramVector.m_z); 
-			}
-
 			inline Vector3D<T> operator - (const Vector3D<T> & paramVector) const
 			{
-				return Vector3D<T>(-paramVector.m_x,-paramVector.m_y,-paramVector.m_z);
+				return Vector3D<T>(m_x - paramVector.m_x, m_y - paramVector.m_y, m_z - paramVector.m_z);
+			}
+
+			inline Vector3D<T> operator -= (const Vector3D<T> & paramVector) const
+			{
+				return Vector3D<T>(-paramVector.m_x, -paramVector.m_y, -paramVector.m_z);
 			}
 
 			/// Operator *= multiplication vector with T paramValue
@@ -130,9 +134,9 @@ namespace prj {
 			/// Operator * multiplication vector with T paramValue
 			/// @param paramValue to use to make the multiplication 
 			/// @result (xa,ya,za)*paramValue => (xa*paramValue,ya*paramValue,za*paramValue)
-			inline Vector3D<T> operator * (const T & paramValue) const 
-			{ 
-				return Vector3D<T>(m_x * paramValue, m_y * paramValue, m_z * paramValue); 
+			inline Vector3D<T> operator * (const T & paramValue) const
+			{
+				return Vector3D<T>(m_x * paramValue, m_y * paramValue, m_z * paramValue);
 			}
 
 			/// Operator /= division vector with T paramValue
@@ -161,9 +165,9 @@ namespace prj {
 			/// Operator * multiplication with two vectors
 			/// @param vector to use to make the multiplication 
 			/// @result (xa,ya,za)*(xb,yb,zb) => (xa*xb,ya*yb,za*zb)
-			inline Vector3D<T> operator * (const Vector3D<T> & paramVector) const 
-			{ 
-				return Vector3D<T>(m_x * paramVector.m_x, m_y * paramVector.m_y, m_z * paramVector.m_z); 
+			inline Vector3D<T> operator * (const Vector3D<T> & paramVector) const
+			{
+				return Vector3D<T>(m_x * paramVector.m_x, m_y * paramVector.m_y, m_z * paramVector.m_z);
 			}
 
 			/// Operator *= multiplication with two vectors
@@ -180,30 +184,30 @@ namespace prj {
 			/// Operator ^ scalar product
 			/// @param vector to use for scalar operation
 			/// @return T (xa,ya)^(xb,yb) = (xa*yb) - (ya*xb)
-			inline T operator ^ (const Vector3D<T> & paramVector) const 
-			{ 
-				return (m_x * paramVector.m_y) - (m_y * paramVector.m_x); 
+			inline T operator ^ (const Vector3D<T> & paramVector) const
+			{
+				return (m_x * paramVector.m_y) - (m_y * paramVector.m_x);
 			}
 
 			/// Operator equality check exact value
 			/// @param source to use for comparison
 			/// @return T (xa,ya,za) == (xb,yb,zb) = (xa == xb) and (ya == yb) and (za == zb)
-			inline bool operator == (const Vector3D<T> & paramVector) 
-			{ 
-				return ((m_x == paramVector.m_x) && (m_y == paramVector.m_y) && (m_z == paramVector.m_z)); 
+			inline bool operator == (const Vector3D<T> & paramVector)
+			{
+				return ((m_x == paramVector.m_x) && (m_y == paramVector.m_y) && (m_z == paramVector.m_z));
 			}
 
 			/// Operator not equal check exact value
 			/// @param source to use for comparison
 			/// @return T (xa,ya,za) != (xb,yb,za) = (xa != xb) or (ya != yb) or (za != zb)		
-			inline bool operator != (const Vector3D<T> & paramVector)	
-			{ 
-				return !this->operator == (paramVector); 
+			inline bool operator != (const Vector3D<T> & paramVector)
+			{
+				return !this->operator == (paramVector);
 			}
 
-			inline T LengthSquared(void) 
-			{ 
-				return m_x * m_x + m_y * m_y + m_z * m_z; 
+			inline T LengthSquared(void)
+			{
+				return m_x * m_x + m_y * m_y + m_z * m_z;
 			}
 
 			static Vector3D<T> Cross(const Vector3D<T> & paramVectorA, const Vector3D<T> & paramVectorB)
@@ -218,72 +222,69 @@ namespace prj {
 			inline Vector3D<T> MakeXYZ(void) { return Vector3D<T>(m_x, m_y, m_z); }
 			inline Vector3D<T> MakeXXX(void) { return Vector3D<T>(m_x, m_x, m_x); }
 			inline Vector3D<T> MakeYYY(void) { return Vector3D<T>(m_y, m_y, m_y); }
-			inline Vector3D<T> MakeYYY(void) { return Vector3D<T>(m_z, m_z, m_z); }
+			inline Vector3D<T> MakeZZZ(void) { return Vector3D<T>(m_z, m_z, m_z); }
 			inline Vector3D<T> MakeYZX(void) { return Vector3D<T>(m_y, m_z, m_x); }
 			inline Vector3D<T> MakeZXY(void) { return Vector3D<T>(m_z, m_x, m_y); }
 			inline Vector3D<T> MakeZYX(void) { return Vector3D<T>(m_z, m_y, m_x); }
 
-			inline Vector2D<T> MakeXY(void) { return Vector2D<T>(m_x, m_y) }
+			/*inline Vector2D<T> MakeXY(void) { return Vector2D<T>(m_x, m_y) }
 			inline Vector2D<T> MakeYX(void) { return Vector2D<T>(m_y, m_x) }
 			inline Vector2D<T> MakeXZ(void) { return Vector2D<T>(m_x, m_z) }
 			inline Vector2D<T> MakeZX(void) { return Vector2D<T>(m_z, m_x) }
 			inline Vector2D<T> MakeYZ(void) { return Vector2D<T>(m_y, m_z) }
-			inline Vector2D<T> MakeZY(void) { return Vector2D<T>(m_z, m_y) }
-			
-			inline T Dot (const Vector3D<T> & paramVectorA, const Vector3D<T> & paramVectorB) 
+			inline Vector2D<T> MakeZY(void) { return Vector2D<T>(m_z, m_y) }*/
+
+			inline T Dot(const Vector3D<T> & paramVectorA, const Vector3D<T> & paramVectorB)
 			{
-			      return (paramVectorA.GetX() * paramVectorB.GetX() + paramVectorA.GetY() * paramVectorB.GetY() + paramVectorA.GetZ() * paramVectorB.GetZ());
-			}			
-			
-			inline T SquaredLength() const 
+				return (paramVectorA.GetX() * paramVectorB.GetX() + paramVectorA.GetY() * paramVectorB.GetY() + paramVectorA.GetZ() * paramVectorB.GetZ());
+			}
+
+			inline T SquaredLength() const
 			{
-			        return (Dot (*this, *this));
+				return (Dot(*this, *this));
 			};
-			
+
 			inline T Length(void) const {
-			        return (T)sqrt (SquaredLength());
-			};			
-			
-			inline T Normalize (void) 
+				return (T)sqrt(SquaredLength());
+			};
+
+			inline T Normalize(void)
 			{
-				T l = Length ();
-				
+				T l = Length();
+
 				if (l == (T) 0.0f)
-				  return (T) 0.0f;
-				  
+					return (T) 0.0f;
+
 				m_X /= l;
 				m_Y /= l;
 				m_Z /= l;
-				
+
 				return l;
-			};	
-			
-			inline Vector3D<T> Normalize (const Vector3D<T> & paramVector) 
+			};
+
+			inline Vector3D<T> Normalize(const Vector3D<T> & paramVector)
 			{
-			    Vector3D<T> r (paramVector);
-			    r.Normalize ();
-			    return r;
+				Vector3D<T> r(paramVector);
+				r.Normalize();
+				return r;
 			}
-			
-			inline T Distance (const Vector3D<T> & paramVectorA, const Vector3D<T> & paramVectorB) 
+
+			inline T Distance(const Vector3D<T> & paramVectorA, const Vector3D<T> & paramVectorB)
 			{
-			    return (paramVectorA-paramVectorB).Length ();
+				return (paramVectorA - paramVectorB).Length();
 			}
-			
-			inline Vector3D<T> operator * (const T & paramValue, const Vector3D<T> & paramVector) 
+
+			friend std::ostream & operator<< (std::ostream & paramOutput, const Vector3D<T> & paramVector)
 			{
-			   return (paramVector * paramValue);
-			}			
-			
-			std::ostream & operator<< (std::ostream & paramOutput, const Vector3D<T> & paramVector) 
-			{
-			  paramOutput << "(" << paramVector.GetX() << ";" << paramVector.GetY() << ";" << paramVector.GetZ() << ")";
-			  return paramOutput;
-			}			
+				paramOutput << "(" << paramVector.GetX() << ";" << paramVector.GetY() << ";" << paramVector.GetZ() << ")";
+				return paramOutput;
+			}
+
 		};
-		
+
 		typedef Vector3D<float> Vector3Dfloat;
 		typedef Vector3D<double> Vector3Ddouble;
-		typedef Vector3D<int> Vector3Dint;		
+		typedef Vector3D<int> Vector3Dint;
 	}
 }
+
