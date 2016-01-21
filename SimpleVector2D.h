6da95a0e9ed38c071126a8281e2412396346c07f
machard <pascal.machard@gmail.com>
2016-01-21 14:45:19 +0100

    Update SimpleVector2D.h

--- SimpleVector2D.h
+++ SimpleVector2D.h
@@ -1,328 +1,189 @@
 #pragma once
 
-#include "stdafx.h"
-#include <ostream>
+#include "math.h"
 
-namespace pma
+namespace prj {
+namespace math {
+	
+/// @class mathematical vector with double (x,y).
+class SimpleVector2D
 {
-	namespace math
+private:
+	/// The x component.
+	double m_x;
+	/// The y component.
+	double m_y;
+public:
+	/// A default constructor.
+	/// @brief x and y are set with 0.0
+	inline SimpleVector2D(void) : m_x(0.0), m_y(0.0) {}
+	
+	/// A constructor with parameter.
+	/// @param x value
+	/// @param y value
+	inline SimpleVector2D(const double & x, const double & y) : m_x(x), m_y(y) {}
+	
+	/// A copy constructor.
+	/// @param source the copy source
+	inline SimpleVector2D(const SimpleVector2D & source)
 	{
-		const unsigned int _X_ = 0;
-		const unsigned int _Y_ = 1;
-		const unsigned int _Z_ = 2;
-		const unsigned int _T_ = 3;
-
-		/// @class mathematical vector with (T,dim) (d1,d2,...,dn).
-		template <typename T, unsigned int dim> class VectorXd
-		{
-		private:
-			T * m_data;
-		public:
-			/// A default constructor.
-			/// @brief create the Tab with T time and dim value (set to 0.0)
-			inline VectorXd(void)
-			{
-				m_data = new T[dim];
-				for (int iPos = 0; iPos < dim; iPos++)
-					m_data[iPos] = (T) 0.0;
-			}
-
-			/// A copy constructor.
-			/// @param source the copy source
-			inline VectorXd(const VectorXd<T, dim> & paramVector)
-			{
-				m_data = new double[dim];
-				for (int iPos = 0; iPos < dim; iPos++)
-					m_data[iPos] = (T)paramVector.m_data[iPos];
-			}
-
-			inline VectorXd(T data[dim])
-			{
-				m_data = new double[dim];
-				for (int iPos = 0; iPos < dim; iPos++)
-					m_data[iPos] = (T)data[iPos];
-			}
-
-			inline ~VectorXd(void)
-			{
-				if (m_data != NULL)
-				{
-					delete[] m_data;
-					m_data = NULL;
-				}
-			}
-
-			/// A affection operator.
-			/// @param p_source the affectation source
-			/// @return the object with new value
-			inline VectorXd<T, dim> & operator = (const VectorXd<T, dim> & paramVector)
-			{
-				if (m_data != NULL)
-					delete[] m_data;
-
-				m_data = new double[dim];
-
-				for (int iPos = 0; iPos < dim; iPos++)
-					m_data[iPos] = (T)paramVector.m_data[iPos];
-
-				return (*this);
-			}
-
-			inline T& operator[] (unsigned int paramIndex) {
-				if (paramIndex > dim - 1)
-					throw range_error("VectorXd operator [] out of range access : ");
-
-				return m_data[paramIndex];
-			};
-
-			inline const T& operator[] (unsigned int paramIndex) const
-			{
-				if (paramIndex > dim - 1)
-					throw range_error("VectorXd operator [] out of range access : ");
-
-				return m_data[paramIndex];
-			};
-
-			/// Compute the abslute vector 3d 
-			/// @result the result vector abs(d1,d2,...,dn) = (abs(d1),abs(d2),...,abs(dn))
-			inline VectorXd<T, dim> abs(void) const
-			{
-				T data[dim];
-
-				for (int iPos = 0; iPos < dim; iPos++)
-					data[iPos] = fabs(m_data[iPos]);
-
-				return VectorXd<T, dim>(data);
-			}
-
-			/// Operator += increment vector with another vector
-			/// @param vector to use for additional operation
-			/// @result (d1,d2,...,dn)+=(e1,e2b,...,en) => (d1+=e1,d2+=e2,..,dn+=en)
-			inline VectorXd<T, dim> & operator += (const VectorXd<T, dim> & paramVector)
-			{
-				for (int iPos = 0; iPos < dim; iPos++)
-					m_data[iPos] += paramVector.m_data[iPos];
-
-				return (*this);
-			}
-
-			/// Operator + add vector with another vector
-			/// @param vector to use for additional operation
-			/// @result (d1,d2,...,dn)+(e1,e2b,...,en) => (d1+e1,d2+e2,..,dn+en)
-			inline VectorXd<T, dim> operator + (const VectorXd<T, dim> & paramVector) const
-			{
-				VectorXd<T, dim> result(*this);
-				return (result += paramVector);
-			}
-
-			/// Operator -= decrement vector with another vector
-			/// @param vector to use for substraction operation
-			/// @result (d1,d2,...,dn)-=(e1,e2b,...,en) => (d1-=e1,d2-=e2,..,dn-=en)
-			inline VectorXd<T, dim> & operator -= (const VectorXd<T, dim> & paramVector)
-			{
-				for (int iPos = 0; iPos < dim; iPos++)
-					m_data[iPos] -= paramVector.m_data[iPos];
-
-				return (*this);
-			}
-
-			/// Operator - add vector with another vector
-			/// @param vector to use for substraction operation
-			/// @result (a1,a2,...,an) - (b1,b2,...,bn) => (a1 - b1,a2 - b2,...,an - bn)
-			inline VectorXd<T, dim> operator - (const VectorXd<T, dim> & paramVector) const
-			{
-				VectorXd<T, dim> result(*this);
-				return (result -= paramVector);
-			}
-
-			/// Operator *= multiplication vector with T paramValue
-			/// @param paramValue to use to make the multiplication 
-			/// @result (a1,a2,...,an) *= v => (a1 * v,a2 * v,...,an * v)
-			inline VectorXd<T, dim> & operator *= (const T & paramValue)
-			{
-				for (int iPos = 0; iPos < dim; iPos++)
-					m_data[iPos] *= paramValue;
-
-				return (*this);
-			}
-
-			/// Operator * multiplication vector with T paramValue
-			/// @param paramValue to use to make the multiplication 
-			/// @result (a1,a2,...,an) * v => (a1 * v,a2 * v,...,an * v)
-			inline VectorXd<T, dim> operator * (const T & paramValue) const
-			{
-				VectorXd<T, dim> result(*this);
-				return (result *= paramValue);
-			}
-
-			/// Operator /= division vector with T paramValue
-			/// @param paramValue to use to make the division 
-			/// @result (xa,ya,za)/=paramValue => (xa/=paramValue,ya/=paramValue,za/=paramValue)
-			inline VectorXd<T, dim> & operator /= (const T & paramValue)
-			{
-				if (paramValue == 0.0)
-					throw;
-
-				for (int iPos = 0; iPos < dim; iPos++)
-					m_data[iPos] /= paramValue;
-
-				return (*this);
-			}
-
-			/// Operator / division vector with T paramValue
-			/// @param paramValue to use to make the division 
-			/// @result (xa,ya,za)/=paramValue => (xa/paramValue,ya/paramValue,za/paramValue)
-			inline VectorXd<T, dim> operator / (const T & paramValue) const
-			{
-				if (paramValue == 0.0)
-					throw;
-
-				VectorXd<T, dim> result(*this);
-				return (result /= paramValue);
-			}
-
-			inline bool operator == (const VectorXd<T,dim> & paramVector)
-			{
-				for (int iPos = 0; iPos < dim; iPos++)
-				{
-					if (m_data[iPos] != paramVector.m_data[iPos])
-						return false;
-				}
-				return true;
-			}
-
-			inline bool operator != (const VectorXd<T, dim> & paramVector)
-			{
-				return !this->operator==(paramVector);
-			}
-
-			friend std::ostream & operator<< (std::ostream & paramOutput, const VectorXd<T, dim> & paramVector)
-			{
-				paramOutput << "(";
-				for (int iPos = 0; iPos < dim; iPos++)
-				{
-					paramOutput << paramVector.m_data[iPos];
-					if (iPos < dim - 1)
-						paramOutput << ";";
-				}
-				paramOutput << ")";
-				return paramOutput;
-			}
-		};
-
-		template <typename T> class Vector1d : public VectorXd < T, 1 >
-		{
-		public:
-			inline Vector1d(T x)
-			{
-				operator[](_X_) = x;
-			}
-
-			inline const T &  getX(void) const { return operator[](_X_); }
-			inline T &  getX(void) { return operator[](_X_); }
-		};
-
-		template <typename T> class Vector2d : public VectorXd < T, 2 >
-		{
-		public :		
-			inline Vector2d(T x, T y)
-			{
-				operator[](_X_) = x;
-				operator[](_Y_) = y;
-			}
-
-			inline const T &  getX(void) const { return operator[](_X_); }
-			inline const T & getY(void) const { return operator[](_Y_); }
-			inline T &  getX(void) { return operator[](_X_); }
-			inline T & getY(void) { return operator[](_Y_); }
-
-			inline T getX2(void) { return (operator[](_X_)*operator[](_X_)); }
-			inline T getY2(void) { return (operator[](_Y_)*operator[](_Y_)); }
-
-			inline Vector2d<T> MakeXY(void) { return Vector2d<T>(getX(), getY()); }
-			inline Vector2d<T> MakeYX(void) { return Vector2d<T>(getY(), getX()); }
-
-			inline Vector1d<T> MakeX(void) { return Vector1d<T>(getX()); }
-			inline Vector1d<T> MakeY(void) { return Vector1d<T>(getY()); }
-
-			inline double operator ^ (const Vector2d<T> & vector) const
-			{
-				return (getX() * vector.getY()) - (getY() * vector.getX());
-			}
-
-			inline T LengthSquared(void)
-			{
-				return getX2() + getY2();
-			}
-
-		};
-
-		template <typename T> class Vector3d : public VectorXd < T, 3 >
-		{
-		public:
-			inline Vector3d(T x, T y, T z)
-			{
-				operator[](_X_) = x;
-				operator[](_Y_) = y;
-				operator[](_Z_) = z;
-			}
-
-			inline const T &  getX(void) const { return operator[](_X_); }
-			inline const T & getY(void) const { return operator[](_Y_); }
-			inline const T & getZ(void) const { return operator[](_Z_); }
-			inline T &  getX(void) { return operator[](_X_); }
-			inline T & getY(void) { return operator[](_Y_); }
-			inline T & getZ(void) { return operator[](_Z_); }
-
-			inline Vector3d<T> MakeXYZ(void) { return Vector3d<T>(getX(), getY(), getZ()); }
-			inline Vector3d<T> MakeXZY(void) { return Vector3d<T>(getX(), getZ(), getY()); }
-			inline Vector3d<T> MakeYXZ(void) { return Vector3d<T>(getY(), getX(), getZ()); }
-			inline Vector3d<T> MakeYZX(void) { return Vector3d<T>(getY(), getZ(), getX()); }
-			inline Vector3d<T> MakeZXY(void) { return Vector3d<T>(getZ(), getX(), getY()); }
-			inline Vector3d<T> MakeZYX(void) { return Vector3d<T>(getZ(), getY(), getX()); }
-
-			inline Vector2d<T> MakeXY(void) { return Vector2d<T>(getX(), getY()); }
-			inline Vector2d<T> MakeXZ(void) { return Vector2d<T>(getX(), getZ()); }
-			inline Vector2d<T> MakeYZ(void) { return Vector2d<T>(getY(), getZ()); }
-
-			inline Vector1d<T> MakeX(void) { return Vector1d<T>(getX()); }
-			inline Vector1d<T> MakeY(void) { return Vector1d<T>(getY()); }
-			inline Vector1d<T> MakeZ(void) { return Vector1d<T>(getZ()); }
-		};
-
-		template <typename T> class Vector4d : public VectorXd < T, 4 >
-		{
-		public:
-			inline Vector4d(T x, T y, T z, T t)
-			{
-				operator[](_X_) = x;
-				operator[](_Y_) = y;
-				operator[](_Z_) = z;
-				operator[](_T_) = t;
-			}
-
-			inline const T &  getX(void) const { return operator[](_X_); }
-			inline const T & getY(void) const { return operator[](_Y_); }
-			inline const T & getZ(void) const { return operator[](_Z_); }
-			inline const T & getT(void) const { return operator[](_T_); }
-			inline T &  getX(void) { return operator[](_X_); }
-			inline T & getY(void) { return operator[](_Y_); }
-			inline T & getZ(void) { return operator[](_Z_); }
-			inline T & getT(void) { return operator[](_T_); }
-
-			inline Vector4d<T> MakeXYZT(void) { return Vector3d<T>(getX(), getY(), getZ(), getT()); }
-
-			inline Vector3d<T> MakeXYZ(void) { return Vector3d<T>(getX(), getY(), getZ()); }
-
-			inline Vector2d<T> MakeXY(void) { return Vector2d<T>(getX(), getY()); }
-			inline Vector2d<T> MakeXZ(void) { return Vector2d<T>(getX(), getZ()); }
-			inline Vector2d<T> MakeYZ(void) { return Vector2d<T>(getY(), getZ()); }
-
-			inline Vector1d<T> MakeX(void) { return Vector1d<T>(getX()); }
-			inline Vector1d<T> MakeY(void) { return Vector1d<T>(getY()); }
-			inline Vector1d<T> MakeZ(void) { return Vector1d<T>(getZ()); }
-			inline Vector1d<T> MakeT(void) { return Vector1d<T>(getT()); }
-		};
+		m_x = source.m_x;
+		m_y = source.m_y;
 	}
-}
+	
+	/// A affection operator.
+	/// @param p_source the affectation source
+	/// @return the object with new value
+	inline SimpleVector2D & operator = (const SimpleVector2D & source)
+	{
+		m_x = source.m_x;
+		m_y = source.m_y;
+		return (*this);
+	}
+	
+	/// A field x accessor.
+	/// @return x value
+	inline const double & GetX(void) const  { return m_x; }
+	/// Set the field x.
+	/// @param value the value set to x
+	inline void SetX(const double & value) { m_x = value; }
+	
+	/// A field y accessor.
+	/// @return y value
+	inline const double & GetY(void)const  { return m_y; }
+	/// Set the field y.
+	/// @param value the value set to y
+	inline void SetY(const double & value) { m_y = value; }
 
+	/// Compute the abslute vector 2d abs(x,y) = (abs(x),abs(y)). 
+	/// @result the result vector 
+	inline SimpleVector2D abs (void) const
+	{
+		return SimpleVector2D(fabs(m_x),fabs(m_y));
+	}
+
+	/// Operator += increment vector with another vector 
+	/// @param vector to use for additional operation
+	/// @result (xa,ya)+=(xb,yb) => (xa+=xb,ya+=yb)
+	inline SimpleVector2D & operator += (const SimpleVector2D & source)
+	{
+		m_x += source.m_x;
+		m_y += source.m_y;
+		return (*this);
+	}
+
+	/// Operator + adds two vectors
+	/// @param vector to use for additional operation
+	/// @return (xa,ya)+(xb,yb) = (xa+xb,ya+yb)
+	inline SimpleVector2D operator + (const SimpleVector2D & source) const
+	{
+		return SimpleVector2D(m_x + source.m_x,m_y + source.m_y);
+	}
+
+	/// Operator -= decrement vector with another vector 
+	/// @param vector to use for sustraction operation
+	/// @result (xa,ya)-=(xb,yb) => (xa-=xb,ya-=yb)
+	inline SimpleVector2D & operator -= (const SimpleVector2D & source)
+	{
+		m_x -= source.m_x;
+		m_y -= source.m_y;
+		return (*this);
+	}
+
+	/// Operator + subb two vectors
+	/// @param vector to use for sustraction operation
+	/// @return (xa,ya)-(xb,yb) = (xa-xb,ya-yb)
+	inline SimpleVector2D operator - (const SimpleVector2D & source) const
+	{
+		return SimpleVector2D(m_x - source.m_x,m_y - source.m_y);
+	}
+
+	/// Operator *= multiplication one vector and a double
+	/// @param double to use for multiplication operation
+	/// @return (x,y)*m => (x*=m,y*=m)
+	inline SimpleVector2D & operator *= (const double & value)
+	{
+		m_x *= value;
+		m_y *= value;
+		return (*this);
+	}
+	
+	/// Operator * multiplication one vector and a double
+	/// @param double to use for multiplication operation
+	/// @return (x,y)*m = (x*m,y*m)
+	inline SimpleVector2D operator * (const double & value) const
+	{
+		return SimpleVector2D(m_x*value, m_y*value);
+	}
+
+	/// Operator /= disision one vector and a double
+	/// @param double to use for division operation
+	/// @return (x,y)/m => (x/=m,y/=m)
+	inline SimpleVector2D & operator /= (const double & value)
+	{
+		if (value == 0.0)
+			throw;
+		m_x /= value;
+		m_y /= value;
+		return (*this);
+	}
+	
+	/// Operator / disision one vector and a double
+	/// @param double to use for disision operation
+	/// @return (x,y)/m = (x/m,y/m)
+	inline SimpleVector2D operator / (const double & value) const
+	{
+		if (value == 0.0)
+			throw;
+		return SimpleVector2D(m_x / p_value, m_y / p_value);
+	}
+
+	/// Operator * multiplication two vectors 
+	/// @param SimpleVector2D to use for multiplication operation
+	/// @return (xa,ya)*(xb,yb) = (xa*xb,ya*yb)
+	inline SimpleVector2D operator * (const SimpleVector2D & source) const
+	{
+		return SimpleVector2D(m_x * source.m_x,m_y * source.m_y);
+	}
+
+	/// Operator * multiplication two vectors 
+	/// @param SimpleVector2D to use for multiplication operation
+	/// @return (xa,ya)*(xb,yb) = (xa*=xb,ya*=yb)
+	inline SimpleVector2D & operator *= (const SimpleVector2D & source)
+	{
+		m_x *= source.m_x;
+		m_y *= source.m_y;
+		return (*this);
+	}
+
+	/// Operator ^ scalar product
+	/// @param SimpleVector2D to use for scalar operation
+	/// @return double (xa,ya)^(xb,yb) = (xa*yb) - (ya*xb)
+	inline double operator ^ (const SimpleVector2D & source) const
+	{
+		return (m_x * source.m_y) - (m_y * source.m_x);
+	}
+
+	/// Operator equality check exact value
+	/// @param SimpleVector2D to use for comparison
+	/// @return double (xa,ya) == (xb,yb) = (xa == xb) and  (ya == yb)
+	inline bool operator == (const SimpleVector2D & source)
+	{
+		return ((m_x == source.m_x) && (m_y == source.m_y));
+	}
+
+	/// Operator not equal check exact value
+	/// @param SimpleVector2D to use for comparison
+	/// @return double (xa,ya) != (xb,yb) = (xa != xb) or (ya != yb)
+	inline bool operator != (const SimpleVector2D & source)
+	{
+		return !this->operator == (source);
+	}
+	
+	inline double LengthSquared (void)
+	{
+		return m_x * m_x + m_y * m_y;
+	}
+};
+}
+}
