c2a6a4d21e34651511611fae3e195186e418ef01
machard <pascal.machard@gmail.com>
2016-01-29 15:41:47 +0100

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -19,15 +19,13 @@ namespace pma
 			inline Matrix()
 			{
 				m_data = new T[dim1*dim2];
-				for (int iPos = 0; iPos < dim1*dim2; iPos++)
-					m_data[iPos] = (T) 0.0;
+				setAll((T)0.0);
 			};
 
 			inline Matrix(T value)
 			{
 				m_data = new T[dim1*dim2];
-				for (int iPos = 0; iPos < dim1*dim2; iPos++)
-					m_data[iPos] = value;
+				setAll(value);
 			};
 
 			inline Matrix(T * ptrValue)
@@ -37,6 +35,15 @@ namespace pma
 				{
 					m_data[iPos] = *ptrValue;
 					ptrValue++;
+				}
+			};
+
+			inline Matrix(const Matrix<T, dim1, dim2> & mat)
+			{
+				m_data = new T[dim1*dim2];
+				for (int iPos = 0; iPos < dim1*dim2; iPos++)
+				{
+					m_data[iPos] = mat.m_data[iPos];
 				}
 			};
 
@@ -53,28 +60,88 @@ namespace pma
 			{
 				m_data[iPos1 * dim2 + iPos2] = value;
 			}
+
+			inline void setAll(const T & value)
+			{
+				for (int iPos = 0; iPos < dim1*dim2; iPos++)
+				{
+					m_data[iPos] = value;
+				}
+			}
+
+			inline void setDiagonal(const T & value)
+			{
+				for (int iPos = 0; iPos < dim1 && iPos < dim2; iPos++)
+				{
+					set(iPos, iPos,value);
+				}
+			}		
+
+			inline void setIdentity()
+			{
+				setAll((T)0.0);
+				setDiagonal((T)1.0);
+			}
+			
+			Matrix<T, dim1, dim2> operator+(const Matrix<T, dim1, dim2> & mat) const
+			{
+				Matrix<T, dim1, dim2> result(*this);
+				return (result += mat);
+			}
+
+			Matrix<T, dim1, dim2> & operator+=(const Matrix<T, dim1, dim2> & mat)
+			{
+				for (int iPos = 0; iPos < dim1*dim2; iPos++)
+				{
+					m_data[iPos] += mat.m_data[iPos];
+				}
+				return (*this);
+			}
+
+			Matrix<T, dim1, dim2> operator-(const Matrix<T, dim1, dim2> & mat) const
+			{
+				Matrix<T, dim1, dim2> result(*this);
+				return (result -= mat);
+			}
+
+			Matrix<T, dim1, dim2> & operator-=(const Matrix<T, dim1, dim2> & mat)
+			{
+				for (int iPos = 0; iPos < dim1*dim2; iPos++)
+				{
+					m_data[iPos] -= mat.m_data[iPos];
+				}
+				return (*this);
+			}
+
+			Matrix<T, dim1, dim2> & operator=(const Matrix<T, dim1, dim2> & mat)
+			{
+				for (int iPos = 0; iPos < dim1*dim2; iPos++)
+				{
+					m_data[iPos] = mat.m_data[iPos];
+				}
+				return (*this);
+			}
+
+			friend std::ostream & operator<< (std::ostream & paramOutput, const Matrix<T, dim1, dim2> & paramMatrix)
+			{
+				paramOutput << "(";
+				for (int iPos1 = 0; iPos1 < dim1; iPos1++)
+				{
+					paramOutput << "(";
+					for (int iPos2 = 0; iPos2 < dim2; iPos2++)
+					{
+						paramOutput << paramMatrix.m_data[iPos1 * dim1 + iPos2];
+						if (iPos2 < dim2 - 1)
+							paramOutput << ";";
+					}
+					paramOutput << ")";
+				}
+				paramOutput << ")";
+				return paramOutput;
+			}
+
 		};
 
-		template <typename T> class Matrix2x3 : public Matrix < T, 2, 3 >
-		{
-		public:
-			inline Matrix2x3() : Matrix < T, 2, 3 >()
-			{
-			};
-
-			inline Matrix2x3(T value) : Matrix < T, 2, 3 >(value)
-			{
-			};
-
-			inline Matrix2x3(T * ptrValue) : Matrix < T, 2, 3 >(ptrValue)
-			{
-			};
-
-			inline ~Matrix2x3()
-			{
-			};
-		};
-		
 		template <typename T> class Matrix2x3 : public Matrix < T, 2, 3 >
 		{
 		public:
@@ -84,16 +151,14 @@ namespace pma
 			inline ~Matrix2x3() {};
 		};
 
-			template <typename T> class Matrix3x3 : public Matrix < T, 3, 3 >
-			{
-			public:
-				inline Matrix3x3() : Matrix < T, 3, 3 >() {};
-				inline Matrix3x3(T value) : Matrix < T, 3, 3 >(value) {};
-				inline Matrix3x3(T * ptrValue) : Matrix < T, 3, 3 >(ptrValue) {};
-				inline ~Matrix3x3() {};
-			};
+		template <typename T> class Matrix3x3 : public Matrix < T, 3, 3 >
+		{
+		public:
+			inline Matrix3x3() : Matrix < T, 3, 3 >() {};
+			inline Matrix3x3(T value) : Matrix < T, 3, 3 >(value) {};
+			inline Matrix3x3(T * ptrValue) : Matrix < T, 3, 3 >(ptrValue) {};
+			inline ~Matrix3x3() {};
+		};
 
-	}
-		
 	}
 }
